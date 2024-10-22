17d8f026a030d2188231957052d3092add2cb5dc
val <130545294+val8elster@users.noreply.github.com>
2024-10-22 14:55:46 +0100

    books are now shown and can be sorted by title, author and rating and searched by more categories. also removed the actual search button. and added a addGenre method to book.

--- src/main/java/com/application/athena/services/TempLogic.java
+++ src/main/java/com/application/athena/services/TempLogic.java
@@ -1,11 +1,31 @@
 package com.application.athena.services;
 
 import com.application.athena.valueobjects.*;
+import com.application.athena.valueobjects.enums.Genre;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TempLogic {
     public Book currentBook;
     public List<Book> bookList;
     public List<Series> seriesList;
+
+    public TempLogic() {
+        bookList = new ArrayList<>();
+        seriesList = new ArrayList<>();
+
+        bookList.add(new Book(12345L));
+        bookList.add(new Book(56789L));
+        bookList.get(0).title = "Book 1";
+        bookList.get(1).title = "Book 2";
+        bookList.get(0).author = "John Doe";
+        bookList.get(1).author = "Jane Doe";
+        bookList.get(0).rating = 1;
+        bookList.get(1).rating = 2;
+
+        bookList.get(0).addGenre(Genre.FANTASY);
+        bookList.get(0).addGenre(Genre.ROMANCE);
+        bookList.get(0).setRead(true);
+    }
 }
